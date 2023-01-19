e342a47a578960a3d9ad9a888a379af923743d31
Cristian Gonzalez Lopez <cristiangl1995@gmail.com>
2023-01-19 18:06:46 +0100

    change bg color

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <Link to={'/'} className='h-28 flex items-center justify-center mb-14 sticky top-0 z-5'>
+    <Link to={'/'} className='h-28 flex items-center justify-center mb-14'>
         <img className='h-28' src={require('../images/Logo.png')} alt='Marvel Snap Logo' />
     </Link>
   )
